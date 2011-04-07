728661ae516b84a1ebad868f28f650fca6fbb36a
NickOFreak <nickoboy@gmail.com>
2011-04-07 12:29:03 +0200

    byter ut rar

---
+++ Rapid Reporter Pegasus 1.0/Rapid Reporter/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Rapid_Reporter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            Logger.record("[App]: >>>> Rapid Reporter Starting", "App", "info");
+        }
+        ~App()
+        {
+            Logger.record("[App]: <<<< Rapid Reporter Ending", "App", "info");
+            Logger.record("[App]: <<<< ====================================", "App", "info");
+        }
+    }
+}
