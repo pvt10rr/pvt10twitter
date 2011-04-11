66ff3b306a09427e570819244a05b8f83eb9a1e6
krsg <slaend@gmail.com>
2011-04-11 13:03:16 +0200

    Update
    Signed-off-by: krsg <slaend@gmail.com>

---
+++ Rapid Reporter Pegasus 1.2/Rapid Reporter/ScreenShot.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+// Code adapted from:
+// http://kseesharp.blogspot.com/2008/11/c-capture-screenshot.html
+// Sent email requesting license to adapt the code and permission to refer to his link.
+//  There were many changes to the code, but still good to keep credit for original.
+//  Althought similar code appears in so many places thet it is hard to point who is the original. Probably Microsoft :).
+
+namespace Rapid_Reporter
+{
+    class ScreenShot
+    {
+        [DllImport("gdi32.dll")]
+        static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, CopyPixelOperation rop);
+        [DllImport("user32.dll")]
+        static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteDC(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr DeleteObject(IntPtr hDc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+        [DllImport("gdi32.dll")]
+        static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr ptr);
+
+        public Bitmap CaptureScreenShot()
+        {
+            Logger.record("[CaptureScreenshot]: Will take a screenshot of the monitor.", "ScreenShot", "info");
+            Rectangle fullBounds = new Rectangle();
+            int minX = 0, minY = 0, maxX = 0, maxY = 0;
+
+            /*
+
+             * Composite desktop calculations for multiple monitors
+
+                A, B            E, B                     ||                  E, B
+                  +--------------+                       ||   *              +---------------+ D, B
+                  |              |E, G                   ||                  |               |
+                  |              +---------------+ D, G  ||   +--------------+ E, C          |
+                  |              |               |       ||   |A, C          |               |
+                  |              |               |       ||   |              |               |
+                  |              |               |       ||   |              |               |
+                  +--------------+ E, C          |       ||   |              +---------------+ D, G
+                A, C             |               |       ||   |              |E, G
+                                 +---------------+ D, F  ||   +--------------+               *
+                                E, F                     ||   A, F           E, F
+
+             * We capture from "A, B" to "D, F".
+             *   That is, we look for the minimum X,Y coordinate first.
+             *   Then we look for the largest width,height.
+ 
+            */
+
+            foreach (Screen oneScreen in Screen.AllScreens)
+            {
+                Logger.record("\t[CaptureScreenshot]: AllScreens[]: " + oneScreen.Bounds.ToString(), "ScreenShot", "info");
+                minX = Math.Min(minX, oneScreen.Bounds.Left);
+                minY = Math.Min(minY, oneScreen.Bounds.Top);
+                maxX = Math.Max(maxX, oneScreen.Bounds.Width + oneScreen.Bounds.X);
+                maxY = Math.Max(maxY, oneScreen.Bounds.Height + oneScreen.Bounds.Y);
+            }
+            fullBounds = new Rectangle(minX, minY, maxX - minX, maxY - minY);
+            Logger.record("[CaptureScreenshot]: fullScreen[]: " + fullBounds.ToString(), "ScreenShot", "info");
+
+            IntPtr hDesk = GetDesktopWindow();
+            IntPtr hSrce = GetWindowDC(hDesk);
+            IntPtr hDest = CreateCompatibleDC(hSrce);
+            // Our bitmap will have the size of the composite screenshot
+            IntPtr hBmp = CreateCompatibleBitmap(hSrce, fullBounds.Width, fullBounds.Height);
+            IntPtr hOldBmp = SelectObject(hDest, hBmp);
+            // We write on coordinate 0,0 of the bitmap buffer, of course. But we write the the fullBoundsX,Y pixels.
+            bool b = BitBlt(hDest, 0, 0, fullBounds.Width, fullBounds.Height, hSrce, fullBounds.X, fullBounds.Y, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+            Bitmap bmp = Bitmap.FromHbitmap(hBmp);
+            SelectObject(hDest, hOldBmp);
+            DeleteObject(hBmp);
+            DeleteDC(hDest);
+            ReleaseDC(hDesk, hSrce);
+            Logger.record("[CaptureScreenshot]: BMP object ready, returning it to calling function", "ScreenShot", "info");
+            return (bmp);
+        }
+    }
+}
---
+++ Rapid Reporter Pegasus 1.2/Rapid Reporter/TwitterAddon.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Imaging;
+using System.Threading;
+using Twitterizer;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Rapid_Reporter
+{
+    public class TwitterAddon
+    {
+        // Post to Twitter enabled (true) or disabled (false)
+        public static bool twitter = true;
+
+        public static OAuthTokens tokens;
+        public static string twitterPIN;
+        public static string ScreenName;
+        public static string hashCode;
+        private static string requestToken;
+        private static decimal UserID;
+
+        // PostOnTwitter variables
+        public static string tempNote;
+        public static string tempTester;
+        public static string tempCharter;
+        public static string[] tempNoteTypes;
+        public static int tempType;
+
+        public static void TwitterLogin()
+        {
+            requestToken = OAuthUtility.GetRequestToken("WxOSthlIyUDQzWiGzY7F5Q", "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM", "oob").Token;
+
+            Process browserProcess = new Process();
+            browserProcess.StartInfo.FileName = OAuthUtility.BuildAuthorizationUri(requestToken).AbsoluteUri;
+            if (browserProcess.Start())
+            {
+                browserProcess.WaitForExit();
+            }
+            else
+            {
+                //Could not start your browser
+            }
+        }
+
+        public static void TwitterOAuth()
+        {
+            OAuthTokenResponse accessToken = OAuthUtility.GetAccessToken("WxOSthlIyUDQzWiGzY7F5Q", "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM", requestToken, TwitterAddon.twitterPIN);
+
+            tokens = new OAuthTokens();
+            tokens.AccessToken = accessToken.Token;
+            tokens.AccessTokenSecret = accessToken.TokenSecret;
+            tokens.ConsumerKey = "WxOSthlIyUDQzWiGzY7F5Q";
+            tokens.ConsumerSecret = "TD3TbCa9BshneTMJTgdTmm57wzfQrQdXa8Ex2Sd7BkM";
+            ScreenName = accessToken.ScreenName;
+            UserID = accessToken.UserId;
+        }
+
+        public static string GetUniqueKey(int length)
+        {
+            string guidResult = string.Empty;
+
+            while (guidResult.Length < length)
+            {
+                // Get the GUID.
+                guidResult += Guid.NewGuid().ToString().GetHashCode().ToString("x");
+            }
+
+            // Make sure length is valid.
+            if (length <= 0 || length > guidResult.Length)
+                throw new ArgumentException("Length must be between 1 and " + guidResult.Length);
+
+            // Return the first length bytes.
+            hashCode = guidResult.Substring(0, length);
+            return hashCode;
+        }
+
+        public static void PostOnTwitter() // Method for truncating message if too long and posting it on Twitter
+        {
+            string twitterPost = "[Reporter: " + tempTester + ", Charter: " + tempCharter + "] " + tempNote + " #" + tempNoteTypes[tempType] + " #" + TwitterAddon.hashCode;
+            string twitterNote;
+            int twitterNoteLength;
+            int twitterNumberOfCharsToRemove;
+
+            if (twitterPost.Length > 140)
+            {
+                twitterNumberOfCharsToRemove = twitterPost.Length - 140; // how many characters to remove
+                twitterNoteLength = tempNote.Length - twitterNumberOfCharsToRemove; // on which character to start removing
+                twitterNote = tempNote.Substring(0, twitterNoteLength);
+                twitterPost = "[Reporter: " + tempTester + ", Charter: " + tempCharter + "] " + twitterNote + " #" + tempNoteTypes[tempType] + " #" + TwitterAddon.hashCode;
+            }
+
+            TwitterResponse<TwitterStatus> tweetResponse = TwitterStatus.Update(TwitterAddon.tokens, twitterPost);
+        }
+    }
+}
